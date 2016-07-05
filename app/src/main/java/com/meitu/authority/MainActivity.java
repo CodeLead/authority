73c62c98d7f64f67c9b154ab648a87d557f304f5
CodeLead <798782866@qq.com>
2016-07-05 19:19:18 +0800

    delete maxSdkVersion

--- app/src/main/java/com/meitu/authority/MainActivity.java
+++ app/src/main/java/com/meitu/authority/MainActivity.java
@@ -1,6 +1,7 @@
 package com.meitu.authority;
 
 import android.content.Intent;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
